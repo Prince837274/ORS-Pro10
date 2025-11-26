23c80479dc31dccee86773385a9f0a1bdae90a59
Prince Bharti <prince837274@gmail.com>
2025-11-26 14:12:08 +0530

    done

---
+++ ORSProj10/Project_10/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * Prince Bharti 
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
